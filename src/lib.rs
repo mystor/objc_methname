7970ea5f022a6706fdaa46f37b48a9c5bf79ccfc
Michael Layzell <michael@thelayzells.com>
2017-01-16 17:26:57 -0500

    First crummy test version

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use proc_macro::TokenStream;
+
+use syn::{Body, VariantData};
+
+#[proc_macro_derive(__objc_methname, attributes(value))]
+pub fn derive_objc_methname(input: TokenStream) -> TokenStream {
+    let source = input.to_string();
+    let ast = syn::parse_macro_input(&source).unwrap();
+
+    // Get the value, it will either be of the form of a single rust identifier,
+    // or a series of rust identifiers ended by a :. The first case will be
+    // handled by the struct name, while the second will be handled by a struct
+    // with dummy fields, each of which is one of the rust identifiers. It's a
+    // hack.
+    let mut value = match ast.body {
+        Body::Struct(VariantData::Unit) =>
+            ast.ident.to_string().into_bytes(),
+        Body::Struct(VariantData::Struct(ref fields)) => {
+            let mut result = Vec::new();
+            for field in fields {
+                result.extend(field.clone().ident.unwrap().to_string().into_bytes());
+                result.push(b':');
+            }
+            result
+        }
+        _ => panic!()
+    };
+
+    // let mut value = ast.ident.to_string().into_bytes();
+    value.push(b'\0'); // Add the null to the end of the string
+    let length = value.len();
+    let name = &ast.ident;
+    let result = quote!{
+        impl #name {
+            #[inline]
+            fn get() -> *const u8 {
+                // Create a wrapper which 
+                struct SendSyncWrap(*const [u8; #length]);
+                unsafe impl Send for SendSyncWrap {}
+                unsafe impl Sync for SendSyncWrap {}
+
+                #[link_section="__TEXT,__objc_methname,cstring_literals"]
+                static VALUE : [u8; #length] = #value;
+                #[link_section="__DATA,__objc_selrefs,literal_pointers,no_dead_strip"]
+                static REF : SendSyncWrap = SendSyncWrap(&VALUE);
+
+                REF.0 as *const u8
+            }
+        }
+    };
+
+    result.to_string().parse().unwrap()
+}
+
---
+++ test/src/main.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate objcstuff;
+
+macro_rules! intern {
+    ($id:ident) => {
+        {
+            #[allow(dead_code, bad_style)]
+            #[derive(__objc_methname)]
+            struct $id;
+            $id::get()
+        }
+    };
+    ($($id:ident :)+) => {
+        {
+            #[allow(dead_code, bad_style)]
+            #[derive(__objc_methname)]
+            struct Dummy {
+                $($id : u32,)+
+            }
+            Dummy::get()
+        }
+    };
+}
+
+fn main() {
+    assert_eq!(intern!(Rustling), intern!(Rustling));
+    assert_eq!(intern!(Apples:pears:), intern!(Apples:pears:));
+}
